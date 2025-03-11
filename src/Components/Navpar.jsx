d358cce6bc146c7ee7eb9e1f5a00033d2a0e3579
abdelruhamn ahmed muhamed <152011044+abdelruhman999@users.noreply.github.com>
2025-03-11 22:31:15 +0200

    Initial commit

--- src/Components/Navpar.jsx
+++ src/Components/Navpar.jsx
@@ -6,11 +6,14 @@ export default function Navpar() {
     <div className='bg-[#1B1A2A]
     w-full font-Inter font-medium text-[18px] text-white
     gap-[32px] p-[15px] text flex justify-center'>
-      <Link
-      to='/' 
-      >
+      <button onClick={() => {
+        const aboutSection = document.getElementById("about-us");
+        if (aboutSection) {
+          aboutSection.scrollIntoView({ behavior: "smooth" });
+        }
+      }}>
         contact us
-      </Link>
+      </button>
       <a className='cursor-pointer' href='/'>about</a>
       <Link
        to='/Sittings'
